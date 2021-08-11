4e694875dd889d0efa7e74993c9f69bc39c618c2
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-11 18:47:16 -0500

    feat: Add eureka server project

---
+++ src/test/java/com/everis/bceurekaserver/BcEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bceurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
